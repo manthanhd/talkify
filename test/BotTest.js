bf8d8f8cf258aac1df82b6c488a9086f0196e26b
Manthan Dave <manthanhd@live.com>
2016-10-17 23:29:17 +0100

    More validation in addSkill function + restructured tests.

--- test/BotTest.js
+++ test/BotTest.js
@@ -53,76 +53,153 @@ describe('Bot', function () {
         done();
     });
 
-    it('passes classifierPreference property from config to ClassifierFactory', function (done) {
-        var naturalMock = {};
+    describe('<init>', function () {
+        it('passes classifierPreference property from config to ClassifierFactory', function (done) {
+            var naturalMock = {};
 
-        mockery.registerMock('natural', naturalMock);
+            mockery.registerMock('natural', naturalMock);
 
-        var mockClassifierFactoryInstance = mockClassifierFactory();
+            var mockClassifierFactoryInstance = mockClassifierFactory();
 
-        var bot = new Bot({classifierPreference: 'naive_bayes'});
+            var bot = new Bot({classifierPreference: 'naive_bayes'});
 
-        expect(mockClassifierFactoryInstance.newClassifier.calls.length).toBe(1);
-        expect(mockClassifierFactoryInstance.newClassifier).toHaveBeenCalledWith(naturalMock, 'naive_bayes');
-        done();
+            expect(mockClassifierFactoryInstance.newClassifier.calls.length).toBe(1);
+            expect(mockClassifierFactoryInstance.newClassifier).toHaveBeenCalledWith(naturalMock, 'naive_bayes');
+            done();
+        });
     });
 
-    it('trains single document', function (done) {
-        var mockClassifier = mockClassifierWithMockClassifierFactory();
+    describe('train', function () {
+        it('trains single document when parameters are valid', function (done) {
+            var mockClassifier = mockClassifierWithMockClassifierFactory();
 
-        var bot = new Bot({classifierPreference: 'naive_bayes'});
-        bot.train('topic', 'text');
+            var bot = new Bot({classifierPreference: 'naive_bayes'});
+            bot.train('topic', 'text');
 
-        expect(mockClassifier.addDocument).toHaveBeenCalledWith('text', 'topic');
-        expect(mockClassifier.train).toHaveBeenCalled();
-        done();
-    });
-
-    it('trains all documents', function (done) {
-        var mockClassifier = mockClassifierWithMockClassifierFactory();
-
-        var bot = new Bot({classifierPreference: 'naive_bayes'});
-        bot.trainAll([{text: 'hello', topic: 'topic'}, {text: 'hello2', topic: 'topic2'}], function (err) {
-            expect(err).toNotExist();
-            expect(mockClassifier.addDocument).toHaveBeenCalledWith('hello', 'topic');
-            expect(mockClassifier.addDocument).toHaveBeenCalledWith('hello2', 'topic2');
+            expect(mockClassifier.addDocument).toHaveBeenCalledWith('text', 'topic');
             expect(mockClassifier.train).toHaveBeenCalled();
             done();
         });
-    });
 
-    it('throws TypeError when documents in trainAll is not an array', function (done) {
-        var mockClassifier = mockClassifierWithMockClassifierFactory();
+        it('throws TypeError when topic is undefined', function (done) {
+            var bot = new Bot();
+            try {
+                bot.train(undefined);
+                done('should have failed.');
+            } catch (e) {
+                expect(e).toBeA(TypeError);
+                done();
+            }
+        });
 
-        var bot = new Bot({classifierPreference: 'naive_bayes'});
-        bot.trainAll('not_an_array', function (err) {
-            expect(err).toExist();
-            expect(err).toBeA(TypeError);
-            done();
+        it('throws TypeError when topic is not String', function (done) {
+            var bot = new Bot();
+            try {
+                bot.train([]);
+                done('should have failed.');
+            } catch (e) {
+                expect(e).toBeA(TypeError);
+                done();
+            }
+        });
+
+        it('throws TypeError when text is undefined', function (done) {
+            var bot = new Bot();
+            try {
+                bot.train('topic', undefined);
+                done('should have failed.');
+            } catch (e) {
+                expect(e).toBeA(TypeError);
+                done();
+            }
+        });
+
+        it('throws TypeError when text is not String', function (done) {
+            var bot = new Bot();
+            try {
+                bot.train('topic', []);
+                done('should have failed.');
+            } catch (e) {
+                expect(e).toBeA(TypeError);
+                done();
+            }
         });
     });
 
-    it('throws TypeError when documents in trainAll is undefined', function (done) {
-        var mockClassifier = mockClassifierWithMockClassifierFactory();
+    describe('trainAll', function () {
 
-        var bot = new Bot({classifierPreference: 'naive_bayes'});
-        bot.trainAll(undefined, function (err) {
-            expect(err).toExist();
-            expect(err).toBeA(TypeError);
+        it('trains all documents when parameters are valid', function (done) {
+            var mockClassifier = mockClassifierWithMockClassifierFactory();
+
+            var bot = new Bot({classifierPreference: 'naive_bayes'});
+            bot.trainAll([{text: 'hello', topic: 'topic'}, {text: 'hello2', topic: 'topic2'}], function (err) {
+                expect(err).toNotExist();
+                expect(mockClassifier.addDocument).toHaveBeenCalledWith('hello', 'topic');
+                expect(mockClassifier.addDocument).toHaveBeenCalledWith('hello2', 'topic2');
+                expect(mockClassifier.train).toHaveBeenCalled();
+                done();
+            });
+        });
+
+        it('throws TypeError when documents is not an array', function (done) {
+            var mockClassifier = mockClassifierWithMockClassifierFactory();
+
+            var bot = new Bot({classifierPreference: 'naive_bayes'});
+            bot.trainAll('not_an_array', function (err) {
+                expect(err).toExist();
+                expect(err).toBeA(TypeError);
+                done();
+            });
+        });
+
+        it('throws TypeError when documents is undefined', function (done) {
+            var mockClassifier = mockClassifierWithMockClassifierFactory();
+
+            var bot = new Bot({classifierPreference: 'naive_bayes'});
+            bot.trainAll(undefined, function (err) {
+                expect(err).toExist();
+                expect(err).toBeA(TypeError);
+                done();
+            });
+        });
+    })
+
+    describe('addSkill', function () {
+
+        it('adds given skill', function (done) {
+            var bot = new Bot();
+
+            const fakeSkillFn = function (context, req, res, next) {
+            };
+
+            const fakeSkill = new Skill('topic', fakeSkillFn);
+            bot.addSkill(fakeSkill);
+
+            const skills = bot.getSkills();
+            expect(skills.length).toBe(1);
             done();
         });
-    });
 
-    it('adds given skill', function (done) {
-        var bot = new Bot();
+        it('throws TypeError when skill is undefined', function (done) {
+            var bot = new Bot();
+            try {
+                bot.addSkill(undefined);
+                done('should have failed.');
+            } catch (e) {
+                expect(e).toBeA(TypeError);
+                done();
+            }
+        });
 
-        const fakeSkillFn = function (context, req, res, next) { };
-
-        const fakeSkill = new Skill('topic', fakeSkillFn);
-        bot.addSkill(fakeSkill);
-
-        const skills = bot.getSkills();
-        expect(skills.length).toBe(1);
-        done();
-    });
+        it('throws TypeError when skill is not of type Skill', function (done) {
+            var bot = new Bot();
+            try {
+                bot.addSkill([]);
+                done('should have failed.');
+            } catch (e) {
+                expect(e).toBeA(TypeError);
+                done();
+            }
+        });
+    })
 });
